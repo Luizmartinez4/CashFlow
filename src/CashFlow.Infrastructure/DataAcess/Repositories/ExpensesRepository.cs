866b77c0651da7c84b566aee331b8595b52a0db8
luiz.martinez <luizantmartinezfilho3@gmail..com>
2025-05-06 09:36:31 -0300

    Colocando classes na infrastructure

---
+++ src/CashFlow.Infrastructure/DataAcess/Repositories/ExpensesRepository.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
+
+namespace CashFlow.Infrastructure.DataAccess.Repositories;
+internal class ExpensesRepository : IExpensesRepository
+{
+    private readonly CashFlowDbContext _dbContent;
+
+    public ExpensesRepository(CashFlowDbContext dbContext)
+    {
+        _dbContent = dbContext;
+    }
+
+    public async Task Add(Expense expense)
+    {
+        await _dbContent.Expenses.AddAsync(expense);
+    }
+}
---
+++ src/CashFlow.Infrastructure/DataAcess/UnityOfWork.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Domain.Repositories;
+
+namespace CashFlow.Infrastructure.DataAccess;
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly CashFlowDbContext _dbContent;
+
+    public UnitOfWork(CashFlowDbContext dbContext)
+    {
+        _dbContent = dbContext;
+    }
+
+    public async Task Commit() => await _dbContent.SaveChangesAsync();
+}
